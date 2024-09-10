0fb5feecec802a6208523c7b1483e55694dbfef1
Azempresas7 <114088131+Azempresas7@users.noreply.github.com>
2024-09-10 18:28:05 -0300

    Fundamentos 005

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const start = () => {
   let count = 1
 
-  while (count <= 10) {
-    console.log(count)
-    count++
+  while (true) {
+    let opcao = 'Sair'
+
+    switch (opcao) {
+      case 'Cadastrar':
+        console.log('Vamos cadastrar')
+        break
+      case 'Listar':
+        console.log('Vamos listar')
+        break
+      case 'Sair':
+        return
+    }
   }
 }
 
