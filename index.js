5b3fdc8bbdb08675624b874549f380157fece90b
Azempresas7 <114088131+Azempresas7@users.noreply.github.com>
2024-09-11 18:25:55 -0300

    Resolve bug de marcadas/desmarcadas

--- index.js
+++ index.js
@@ -24,14 +24,14 @@ const listGoals = async () => {
     instructions: false
   })
 
+  metas.forEach(meta => {
+    meta.checked = false
+  })
+
   if (answers.length === 0) {
     console.log('Nenhuma meta selecionada!')
     return
   }
-
-  metas.forEach(meta => {
-    meta.checked = false
-  })
 
   answers.forEach(answer => {
     const meta = metas.find(m => {
